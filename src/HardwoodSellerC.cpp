54a4314c15bba4b8fa96e341c1795590a1798a4d
gpita <gabriela_pita@yahoo.com>
2016-01-27 15:34:21 -0500

    implement vector

--- src/HardwoodSellerC.cpp
+++ src/HardwoodSellerC.cpp
@@ -18,14 +18,16 @@ using namespace std;
 int main() {
 	
 	string buyerName, address, date, woodType, amountStr;
-	int amount;
+	int amount, price;
+	char c;
+	vector<WoodItem> wood;
 
-	WoodItem Cherry = new WoodItem("Cherry", 2.5, 5.95);
-	WoodItem CurlyMaple = new WoodItem("Curly Maple", 1.5, 6.00);
-	WoodItem GenuineMahogany = new WoodItem("Genuine Mahogany", 3, 9.60);
-	WoodItem Wenge = new WoodItem("Wenge", 5, 22.35);
-	WoodItem WhiteOak = new WoodItem("White Oak", 2.3, 6.70);
-	WoodItem Sawdust = new WoodItem("Sawdust", 1, 1.5);
+	wood.insert(new WoodItem("Cherry", 2.5, 5.95));
+	wood.insert(new WoodItem("Curly Maple", 1.5, 6.00));
+	wood.insert(new WoodItem("Genuine Mahogany", 3, 9.60));
+	wood.insert(new WoodItem("Wenge", 5, 22.35));
+	wood.insert(new WoodItem("White Oak", 2.3, 6.70));
+	wood.insert(new WoodItem("Sawdust", 1, 1.50));
 	
 	ifstream inputFile;
 	inputFile.open("order.txt");
@@ -51,16 +53,38 @@ int main() {
 
 			c = inputFile.get();
 
-			while (ch != ';') 
+			while (c != ';') 
 			{
 				amountStr += c;
 				c = inputFile.get();
 			}
 
 			amount = stoi(amountStr);
+
+			cout << "Ordered " << amount << " BF of " << woodType << " at price ";
+			switch woodType
+			{
+				case "Cherry": 
+					price = 5.95; baseDeliveryTime = 2.5;
+					break;
+				case "Curly Maple": price = 6.00; baseDeliveryTime = 1.5;
+					break;
+				case "Genuine Mahogany": price = 9.60; baseDeliveryTime = 3;
+					break;
+				case "Wenge": price = 22.35; baseDeliveryTime = 5;
+					break;
+				case "White Oak": price = 6.70; baseDeliveryTime = 2.3;
+					break;
+				case "Sawdust": price = 1.5; baseDeliveryTime = 1;
+					break;
+			}
+
+			cout << price << endl;
 		} 
 
 	inputFile.close();
+
+
 
 	return 0;
 }
@@ -72,21 +96,6 @@ double deliveryTime(string woodType) {
 
 	double deliveryETA = 0.0;
 
-	switch woodType
-	{
-		case "Cherry": 
-			wood.price = 5.95; wood.baseDeliveryTime = 2.5;
-			break;
-		case "Curly Maple": wood.price = 6.00; wood.baseDeliveryTime = 1.5;
-			break;
-		case "Genuine Mahogany": wood.price = 9.60; wood.baseDeliveryTime = 3;
-			break;
-		case "Wenge": wood.price = 22.35; wood.baseDeliveryTime = 5;
-			break;
-		case "White Oak": wood.price = 6.70; wood.baseDeliveryTime = 2.3;
-			break;
-		case "Sawdust": wood.price = 1.5; wood.baseDeliveryTime = 1;
-	}
 
 	if(amount > 0 && amount < 101)
 		deliveryETA = wood.baseDeliveryTime;
